4114adc7be7d6fb521d1fd0c232fbcf8c3d8a489
Art <ya.Artriches@ya.ru>
2021-02-11 11:22:26 +0300

    add: navigation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Index() {
-  return <h1>Index Page</h1>;
+  return (
+    <div>
+      <h1>Index Page</h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  );
 }
