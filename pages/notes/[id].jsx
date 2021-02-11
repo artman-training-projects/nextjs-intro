37eecb591e1f146b55eecb125602538a7bb005bc
Art <ya.Artriches@ya.ru>
2021-02-11 11:22:26 +0300

    add: navigation

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,8 +1,17 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
 
 export default function Note() {
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>Note: {id} </h1>;
+  return (
+    <div>
+      <h1>Note: {id} </h1>
+
+      <Link href="/notes">
+        <a>Notes</a>
+      </Link>
+    </div>
+  );
 }
--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
 export default function Notes() {
-  return <h1>Notes</h1>;
+  const notes = new Array(15)
+    .fill("")
+    .map((e, i) => ({ id: i, title: `Notes: ${i}` }));
+
+  return (
+    <div>
+      <h1>Notes</h1>
+
+      {notes.map((note) => (
+        <div>
+          <Link key={notes.id} href="/notes/[id]" as={`/notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
